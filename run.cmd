713fee1203051669022854027f3e55a290890bb7
IndianTechSupp <90865605+IndianTechSupp7@users.noreply.github.com>
2025-11-30 20:38:05 +0100

    run

---
+++ run.cmd
@@ -0,0 +1,14 @@
+@echo off
+
+if not exist ".venv" (
+    echo Creating virtual environment...
+    python -m venv .venv
+    call .venv\Scripts\activate.bat
+    pip install -r requirements.txt
+) else (
+    call .venv\Scripts\activate.bat
+)
+
+py main.py
+
+cmd
